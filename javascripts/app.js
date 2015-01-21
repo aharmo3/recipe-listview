12602e7fcd81b2f7d9b1a549a6208dc1bdee1bef
Amber Harmon <aharmo3@gmail.com>
2015-01-21 15:21:32 -0800

    updating styles

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,26 +1,25 @@
-
 var app = angular.module("recipeListView", []);
 
 var recipeDataController = function($scope, $http, $log) {
 
-	var onSearchComplete = function(response) {
-		$log.info($scope.loadingClass);
-		$scope.recipes = response.data.matches;
-	};
+    var onSearchComplete = function(response) {
+        $log.info($scope.loadingClass);
+        $scope.recipes = response.data.matches;
+    };
 
-	var onError = function(response){
-		$scope.error = 'Whoops!, something went wrong.'
-	};
-	
-	$scope.search = function(term) {
-		$log.info('searching for  ');
-		$http.get('http://api.yummly.com/v1/api/recipes?_app_id=46ac308e&_app_key=16cf1340c02c27af105b7618247c5e16&maxResult=50&q=' + term)
-			.then(onSearchComplete, onError);
-	};
-	// Create Recipe Url
+    var onError = function(response) {
+        $scope.error = 'Whoops!, something went wrong.'
+    };
+
+    $scope.search = function(term) {
+        $log.info('searching for  ');
+        $http.get('http://api.yummly.com/v1/api/recipes?_app_id=46ac308e&_app_key=16cf1340c02c27af105b7618247c5e16&maxResult=50&q=' + term)
+            .then(onSearchComplete, onError);
+    };
+    // Create Recipe Url
     $scope.recipeUrl = 'http://www.yummly.com/recipe/';
-    
-	$scope.sgSortOrder = '-rating';
+
+    $scope.sgSortOrder = '-rating';
 
 };
 
